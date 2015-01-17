8d76deaffaa0f24ecf2b27a50cda1a6e5bd393f7
Jeremy_Wu <wushuaiyi@outlook.com>
2015-01-17 10:08:13 +0800

    Create Global.cpp

---
+++ Global.cpp
@@ -0,0 +1,31 @@
+// Global.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "KCSJ_HMIS.h"
+#include "Global.h"
+
+
+// Global
+
+IMPLEMENT_DYNAMIC(Global, CWnd)
+
+Global::Global()
+{
+
+}
+
+Global::~Global()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(Global, CWnd)
+END_MESSAGE_MAP()
+
+
+
+// Global 消息处理程序
+
+
+CString Message;
