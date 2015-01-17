9ed672457479a7409f94d9b01232eae63f68daea
Jeremy_Wu <wushuaiyi@outlook.com>
2015-01-17 09:57:41 +0800

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,81 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 KCSJ_HMIS.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_KCSJ_HMIS_DIALOG            102
+#define IDD_KCSJ_CUSTOMER               103
+#define IDD_KCSJ_ROOM                   104
+#define IDD_KCSJ_EMPLOYEE               105
+#define IDD_KCSJ_BOOK                   106
+#define IDD_KCSJ_SERVE                  107
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     130
+#define IDR_MENU1                       132
+#define IDB_BITMAP2                     135
+#define IDB_BITMAP3                     138
+#define IDI_ICON1                       139
+#define IDI_ICON2                       140
+#define IDD_KCSJ_ACCOUNT                141
+#define IDD_KCSJ_ABOUT                  142
+#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1001
+#define IDC_BUTTON3                     1002
+#define IDC_BUTTON4                     1003
+#define IDC_BUTTON5                     1004
+#define IDC_LIST1                       1005
+#define IDC_EDIT1                       1006
+#define IDC_EDIT2                       1007
+#define IDC_EDIT3                       1008
+#define IDC_EDIT4                       1009
+#define IDC_EDIT5                       1010
+#define IDC_RADIO1                      1011
+#define IDC_RADIO2                      1012
+#define IDC_RADIO3                      1013
+#define IDC_RADIO4                      1014
+#define IDC_RADIO5                      1015
+#define IDC_EDIT6                       1017
+#define IDC_RADIO6                      1018
+#define IDC_EDIT7                       1019
+#define IDC_BUTTON6                     1020
+#define IDC_BUTTON7                     1021
+#define IDC_STATIC7                     1022
+#define IDC_STATIC8                     1023
+#define IDC_STATIC9                     1024
+#define IDC_STATIC10                    1025
+#define IDC_STATIC11                    1026
+#define IDC_EDIT8                       1027
+#define IDC_EDIT9                       1028
+#define IDC_EDIT10                      1029
+#define IDC_EDIT12                      1031
+#define IDC_EDIT13                      1032
+#define IDC_EDIT14                      1033
+#define IDC_EDIT11                      1034
+#define IDC_BUTTON8                     1035
+#define IDC_BUTTON9                     1036
+#define IDC_LIST2                       1037
+#define IDC_BUTTON10                    1038
+#define IDC_STATIC4                     1039
+#define IDC_STATIC5                     1040
+#define IDC_STATIC3                     1041
+#define IDC_STATIC2                     1042
+#define IDC_STATIC1                     1043
+#define ID_32771                        32771
+#define ID_MORE                         32772
+#define ID_MORE_INFORMATION             32773
+#define ID_INSTRUCTION                  32774
+#define ID_ABOUT                        32775
+#define ID_ABOUT_ABOUT                  32776
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        143
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1044
+#define _APS_NEXT_SYMED_VALUE           108
+#endif
+#endif
